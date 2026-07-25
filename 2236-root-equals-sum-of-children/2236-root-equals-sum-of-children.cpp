0a9075cff4b24c1e6895c283104b6fad1ea59bb3
Sonu Kumar <sonukr96710@gmail.com>
2026-07-26 00:40:28 +0530

    Time: 0 ms (100%), Space: 15.1 MB (32.27%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -14,27 +14,7 @@ public:
     
     bool checkTree(TreeNode* root) {
         
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            TreeNode* node = q.front();
-            q.pop();
-            int left = 0;
-            int right = 0;
-            if(node->left){
-                q.push(node->left);
-                left = node->left->val;
-            }
-            if(node->right){
-                q.push(node->right);
-                right = node->right->val;
-            }
-            if(node->left != NULL && node->right != NULL){
-                if(node->val != left+right) return false;
-            }
-        }
-
-        return true;
+        return root->val != (root->left->val + root->right->val) ? false : true;
+        
     }
 };
