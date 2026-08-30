b576208ab9a9f8f9e9400e6f4f3d365a56fc6516
Sonu Kumar <sonukr96710@gmail.com>
2026-08-30 07:29:22 +0530

    Time: 32 ms (64.04%), Space: 72.5 MB (44.67%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         priority_queue<int, vector<int>, greater<int>> min_Heap;
         for(int i = 0; i < nums.size(); ++i){
             min_Heap.push(nums[i]);
-            while(min_Heap.size() > k){
+            if(min_Heap.size() > k){
                 min_Heap.pop();
             }
         }
