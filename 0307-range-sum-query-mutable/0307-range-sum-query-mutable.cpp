8c4db69d6c6bcb34ffc82e81a08a223c51090d08
Sonu Kumar <sonukr96710@gmail.com>
2026-09-03 08:10:20 +0530

    Time: 61 ms (53.4%), Space: 182.2 MB (53.89%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -0,0 +1,72 @@
+
+class NumArray {
+public:
+    vector<int> seg;
+    int n;
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        if(n > 0){
+            seg.resize(4*n);
+            buildSegmentTree(nums, seg, 0, 0, n-1);
+
+        }
+    }
+    void buildSegmentTree(vector<int> &arr, vector<int> &seg, int ind, int low, int high){
+        if(high == low){
+            seg[ind] = arr[low];
+            return;
+        }
+
+        int mid = (low+high)/2;
+        buildSegmentTree(arr, seg, 2LL*ind+1, low, mid);
+        buildSegmentTree(arr, seg, 2LL*ind+2, mid+1, high);
+
+        seg[ind] = seg[2*ind+1] + seg[2*ind+2];
+    }
+    void updateSegment(int ind, int index, int val, int low, int high, vector<int> &seg){
+        if(high == low){
+            seg[ind] = val;
+            return;
+        }
+
+        int mid = (low+high)/2;
+        if(index <= mid)
+            updateSegment(2LL*ind+1, index, val, low, mid, seg);
+        else
+            updateSegment(2LL*ind+2, index , val, mid+1, high, seg);
+
+        seg[ind] = seg[2LL*ind+1]+seg[2LL*ind+2];
+    }
+    void update(int index, int val) {
+        updateSegment(0, index, val, 0, n-1, seg);
+    }
+    
+    int sumOfSegment(int ind, int low, int high, int l, int r){
+        // no overlap
+        
+        if (r < low || l > high) {
+            return 0;
+        }
+
+        // complete overlap
+        if(l <= low && high <= r) return seg[ind];
+
+        int mid = (low+high)/2;
+        int left = sumOfSegment(2LL*ind+1, low, mid, l, r);
+        int right = sumOfSegment(2LL*ind+2, mid+1, high, l, r);
+
+        return left+right;
+    }
+    int sumRange(int left, int right) {
+        // no overlap
+        if(n==0) return 0;
+        return sumOfSegment(0, 0, n-1, left, right);
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
